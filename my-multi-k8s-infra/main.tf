cbd2a5acc5604858f112941b51123cb4075be82d
Tamir Baruch <tamirb10@gmail.com>
2026-03-31 10:09:31 +0300

    deploy

--- my-multi-k8s-infra/main.tf
+++ my-multi-k8s-infra/main.tf
@@ -4,7 +4,14 @@ module "vpc" {
 
   name = "multi-k8s-vpc"
   cidr = "10.0.0.0/16"
+# תגיות חובה כדי שאמזון תדע איפה להקים את ה-ALB
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
 
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
   azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]

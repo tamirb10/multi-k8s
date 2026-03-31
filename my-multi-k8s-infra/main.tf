5ca0445ba8197c71fc856cc4b9c5c2ea0d4d0fec
Tamir Baruch <tamirb10@gmail.com>
2026-03-31 09:33:18 +0300

    deploy

--- my-multi-k8s-infra/main.tf
+++ my-multi-k8s-infra/main.tf
@@ -1,4 +1,3 @@
-# 1. יצירת רשת (VPC) מאובטחת
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.0.0"
@@ -10,47 +9,85 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  enable_nat_gateway = true # נדרש כדי ש-Fargate יוכל למשוך אימג'ים מהאינטרנט
-  single_nat_gateway = true # חוסך עלויות (FinOps!) כי לא צריך NAT לכל Zone
+  enable_nat_gateway = true
+  single_nat_gateway = true 
 }
+# זה המודול שמחליף את כל ה-JSON הידני והכאב ראש
 
-# 2. יצירת ה-EKS במצב Fargate (Serverless)
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "18.31.0" # הגרסה היציבה
+  version = "~> 20.31" # גרסה חדשה שתומכת ב-Auto Mode
 
   cluster_name    = "multi-k8s-cluster"
-  cluster_version = "1.29"
+  cluster_version = "1.31"
 
+  bootstrap_self_managed_addons = false
+  create_kms_key              = false
+  cluster_encryption_config   = {}
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  # הגדרת Fargate בגרסה 18
-  fargate_profiles = {
-    default = {
-      name = "default"
-      selectors = [
-        { namespace = "default" },
-        { namespace = "kube-system" }
-      ]
-    }
+  # הגדרות האוטופיילוט:
+  cluster_compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
   }
-}
 
-module "lb_controller_role" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "~> 5.0"
-
-  create_role      = true
-  role_name        = "AmazonEKSLoadBalancerControllerRole"
   
-  # מחבר את ה-Role לקלאסטר שלך דרך ה"תעודת זהות" (OIDC)
-  provider_url     = module.eks.oidc_provider 
-
-  # כאן אנחנו מחברים את ה-Policy שיצרת ידנית בשלב 1
-  # שים לב: תחליף את ה-ACCOUNT_ID במספר החשבון שלך
-  role_policy_arns = ["arn:aws:iam::650251704539:policy/AWSLoadBalancerControllerIAMPolicy"]
-
-  # אבטחה: רק הקונטרולר בתוך הקלאסטר רשאי להשתמש בזה
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
+  enable_cluster_creator_admin_permissions = true
 }
+
+
+# --- 3. יצירת ה-Role (תעודת הזהות) ---
+resource "aws_iam_role" "lb_controller" {
+  name = "EKS-LoadBalancer-Role-Auto"
+
+  # יחסי אמון: ה-Role סומך על הקלאסטר (באמצעות ה-OIDC)
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Effect = "Allow"
+      Principal = { Federated = module.eks.oidc_provider_arn }
+      Condition = {
+        StringEquals = {
+          "${replace(module.eks.cluster_oidc_issuer_url, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+        }
+      }
+    }]
+  })
+}
+
+# הצמדת ה"רשימת הרשאות" (Policy) ל-Role
+resource "aws_iam_role_policy_attachment" "lb_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy" # הרשאה בסיסית
+  role       = aws_iam_role.lb_controller.name
+}
+
+# הרשאות ספציפיות ליצירת Load Balancers (החלק הקריטי!)
+resource "aws_iam_role_policy" "lbc_extra" {
+  name = "LBC-Extra-Permissions"
+  role = aws_iam_role.lb_controller.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "ec2:DescribeAccountAttributes",
+        "ec2:DescribeAddresses",
+        "ec2:DescribeInternetGateways",
+        "elasticloadbalancing:*"
+      ]
+      Resource = "*"
+    }]
+  })
+}
+
+# --- 4. ה"דבק" (Access Entry) - החיבור הסופי שמונע לולאה ---
+resource "aws_eks_access_entry" "lb_controller" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = aws_iam_role.lb_controller.arn
+  type          = "EC2_LINUX"
+}
+
